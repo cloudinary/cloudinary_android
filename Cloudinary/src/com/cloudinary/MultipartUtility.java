fe98079904e97c62785cfa622300aa1a1434fddf
Tal Lev-Ami <tal.levami@gmail.com>
2014-07-23 09:32:08 +0300

    Use Chunked transfer mode to prevent local buffering

--- Cloudinary/src/com/cloudinary/MultipartUtility.java
+++ Cloudinary/src/com/cloudinary/MultipartUtility.java
@@ -19,103 +19,103 @@ import java.net.URLConnection;
  * @author Cloudinary
  */
 public class MultipartUtility {
-	private final String boundary;
-	private static final String LINE_FEED = "\r\n";
-	private HttpURLConnection httpConn;
-	private String charset;
-	private OutputStream outputStream;
-	private PrintWriter writer;
+    private final String boundary;
+    private static final String LINE_FEED = "\r\n";
+    private HttpURLConnection httpConn;
+    private String charset;
+    private OutputStream outputStream;
+    private PrintWriter writer;
 
-	/**
-	 * This constructor initializes a new HTTP POST request with content type is
-	 * set to multipart/form-data
-	 * 
-	 * @param requestURL
-	 * @param charset
-	 * @throws IOException
-	 */
-	public MultipartUtility(String requestURL, String charset, String boundary) throws IOException {
-		this.charset = charset;
-		this.boundary = boundary;
+    /**
+     * This constructor initializes a new HTTP POST request with content type is
+     * set to multipart/form-data
+     * 
+     * @param requestURL
+     * @param charset
+     * @throws IOException
+     */
+    public MultipartUtility(String requestURL, String charset, String boundary) throws IOException {
+        this.charset = charset;
+        this.boundary = boundary;
 
-		URL url = new URL(requestURL);
-		httpConn = (HttpURLConnection) url.openConnection();
-		httpConn.setDoOutput(true); // indicates POST method
-		httpConn.setDoInput(true);
-		httpConn.setRequestProperty("Content-Type", "multipart/form-data; boundary=" + boundary);
-		httpConn.setRequestProperty("User-Agent", Cloudinary.USER_AGENT);
-		outputStream = httpConn.getOutputStream();
-		writer = new PrintWriter(new OutputStreamWriter(outputStream, charset), true);
-	}
+        URL url = new URL(requestURL);
+        httpConn = (HttpURLConnection) url.openConnection();
+        httpConn.setDoOutput(true); // indicates POST method
+        httpConn.setDoInput(true);
+        httpConn.setChunkedStreamingMode(0); // Chunked transfer mode to prevent local buffering
+        httpConn.setRequestProperty("Content-Type", "multipart/form-data; boundary=" + boundary);
+        httpConn.setRequestProperty("User-Agent", Cloudinary.USER_AGENT);
+        outputStream = httpConn.getOutputStream();
+        writer = new PrintWriter(new OutputStreamWriter(outputStream, charset), true);
+    }
 
-	/**
-	 * Adds a form field to the request
-	 * 
-	 * @param name
-	 *            field name
-	 * @param value
-	 *            field value
-	 */
-	public void addFormField(String name, String value) {
-		writer.append("--" + boundary).append(LINE_FEED);
-		writer.append("Content-Disposition: form-data; name=\"" + name + "\"").append(LINE_FEED);
-		writer.append("Content-Type: text/plain; charset=" + charset).append(LINE_FEED);
-		writer.append(LINE_FEED);
-		writer.append(value).append(LINE_FEED);
-		writer.flush();
-	}
+    /**
+     * Adds a form field to the request
+     * 
+     * @param name
+     *            field name
+     * @param value
+     *            field value
+     */
+    public void addFormField(String name, String value) {
+        writer.append("--" + boundary).append(LINE_FEED);
+        writer.append("Content-Disposition: form-data; name=\"" + name + "\"").append(LINE_FEED);
+        writer.append("Content-Type: text/plain; charset=" + charset).append(LINE_FEED);
+        writer.append(LINE_FEED);
+        writer.append(value).append(LINE_FEED);
+        writer.flush();
+    }
 
-	/**
-	 * Adds a upload file section to the request
-	 * 
-	 * @param fieldName
-	 *            name attribute in <input type="file" name="..." />
-	 * @param uploadFile
-	 *            a File to be uploaded
-	 * @throws IOException
-	 */
-	public void addFilePart(String fieldName, File uploadFile) throws IOException {
-		String fileName = uploadFile.getName();
-		FileInputStream inputStream = new FileInputStream(uploadFile);
-		addFilePart(fieldName, inputStream, fileName);
-	}
+    /**
+     * Adds a upload file section to the request
+     * 
+     * @param fieldName
+     *            name attribute in <input type="file" name="..." />
+     * @param uploadFile
+     *            a File to be uploaded
+     * @throws IOException
+     */
+    public void addFilePart(String fieldName, File uploadFile) throws IOException {
+        String fileName = uploadFile.getName();
+        FileInputStream inputStream = new FileInputStream(uploadFile);
+        addFilePart(fieldName, inputStream, fileName);
+    }
 
-	public void addFilePart(String fieldName, InputStream inputStream, String fileName) throws IOException {
-		writer.append("--" + boundary).append(LINE_FEED);
-		writer.append("Content-Disposition: form-data; name=\"" + fieldName + "\"; filename=\"" + fileName + "\"").append(LINE_FEED);
-		writer.append("Content-Type: " + URLConnection.guessContentTypeFromName(fileName)).append(LINE_FEED);
-		writer.append("Content-Transfer-Encoding: binary").append(LINE_FEED);
-		writer.append(LINE_FEED);
-		writer.flush();
+    public void addFilePart(String fieldName, InputStream inputStream, String fileName) throws IOException {
+        writer.append("--" + boundary).append(LINE_FEED);
+        writer.append("Content-Disposition: form-data; name=\"" + fieldName + "\"; filename=\"" + fileName + "\"").append(LINE_FEED);
+        writer.append("Content-Type: " + URLConnection.guessContentTypeFromName(fileName)).append(LINE_FEED);
+        writer.append("Content-Transfer-Encoding: binary").append(LINE_FEED);
+        writer.append(LINE_FEED);
+        writer.flush();
 
-		byte[] buffer = new byte[4096];
-		int bytesRead = -1;
-		while ((bytesRead = inputStream.read(buffer)) != -1) {
-			outputStream.write(buffer, 0, bytesRead);
-		}
-		outputStream.flush();
-		inputStream.close();
+        byte[] buffer = new byte[4096];
+        int bytesRead = -1;
+        while ((bytesRead = inputStream.read(buffer)) != -1) {
+            outputStream.write(buffer, 0, bytesRead);
+        }
+        outputStream.flush();
+        inputStream.close();
 
-		writer.append(LINE_FEED);
-		writer.flush();
-	}
+        writer.append(LINE_FEED);
+        writer.flush();
+    }
 
-	public void addFilePart(String fieldName, InputStream inputStream) throws IOException {
-		addFilePart(fieldName, inputStream, "file");
-	}
+    public void addFilePart(String fieldName, InputStream inputStream) throws IOException {
+        addFilePart(fieldName, inputStream, "file");
+    }
 
-	/**
-	 * Completes the request and receives response from the server.
-	 * 
-	 * @return a list of Strings as response in case the server returned status
-	 *         OK, otherwise an exception is thrown.
-	 * @throws IOException
-	 */
-	public HttpURLConnection execute() throws IOException {
-		writer.append("--" + boundary + "--").append(LINE_FEED);
-		writer.close();
+    /**
+     * Completes the request and receives response from the server.
+     * 
+     * @return a list of Strings as response in case the server returned status
+     *         OK, otherwise an exception is thrown.
+     * @throws IOException
+     */
+    public HttpURLConnection execute() throws IOException {
+        writer.append("--" + boundary + "--").append(LINE_FEED);
+        writer.close();
 
-		return httpConn;
-	}
-
+        return httpConn;
+    }
 }
