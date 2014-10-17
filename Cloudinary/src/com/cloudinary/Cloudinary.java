28afc347fd5c39abc857209d065d78cc9398b40b
Karl Åström <karl.astrom@gmail.com>
2014-10-17 14:20:11 +0200

    Add Builder for Configuration objects

--- Cloudinary/src/com/cloudinary/Cloudinary.java
+++ Cloudinary/src/com/cloudinary/Cloudinary.java
@@ -88,6 +88,100 @@ public class Cloudinary {
 			this.shorten = asBoolean(config.get("shorten"), false);			
 			this.uploadPrefix = (String) config.get("upload_prefix");
 		}
+
+		/**
+		 * Build a new {@link Cloudinary.Configuration}
+		 */
+		public static class Builder {
+			private Configuration conf;
+
+			public Builder() { this.conf = new Configuration(); }
+
+			/**
+			 * Creates a {@link Configuration} with the arguments supplied to this builder
+			 */
+			public Configuration build() { return this.conf; }
+
+			/**
+			 * The unique name of your cloud at Cloudinary
+			 * You can find your cloud name in the Account Details section in the dashboard of Cloudinary Management Console.
+			 */
+			public Builder setCloudName(String cloudName) {
+				this.conf.cloudName = cloudName;
+				return this;
+			}
+
+			/**
+			 * API Key
+			 * You can find API Key in the Account Details section in the dashboard of Cloudinary Management Console.
+			 */
+			public Builder setApiKey(String apiKey) {
+				this.conf.apiKey = apiKey;
+				return this;
+			}
+
+			/**
+			 * API Secret
+			 * You can find API Secret in the Account Details section in the dashboard of Cloudinary Management Console.
+			 */
+			public Builder setApiSecret(String apiSecret) {
+				this.conf.apiSecret = apiSecret;
+				return this;
+			}
+
+			/**
+			 * The domain name of the CDN distribution to use for building HTTPS URLs.
+			 * Relevant only for Advanced plan's users that have a private CDN distribution.
+			 */
+			public Builder setSecureDistribution(String secureDistribution) {
+				this.conf.secureDistribution = secureDistribution;
+				return this;
+			}
+
+			/**
+			 * Custom domain name to use for building HTTP URLs.
+			 * Relevant only for Advanced plan's users that have a private CDN distribution and a custom CNAME.
+			 */
+			public Builder setCname(String cname) {
+				this.conf.cname = cname;
+				return this;
+			}
+
+			/**
+			 * Force HTTPS URLs of images even if embedded in non-secure HTTP pages.
+			 */
+			public Builder setSecure(boolean secure) {
+				this.conf.secure = secure;
+				return this;
+			}
+
+			/**
+			 * Should be set to true for Advanced plan's users that have a private CDN distribution.
+			 */
+			public Builder setPrivateCdn(boolean privateCdn) {
+				this.conf.privateCdn privateCdn;
+				return this;
+			}
+
+			/**
+			 * Whether to automatically build URLs with multiple CDN sub-domains.
+			 */
+			public Builder setCdnSubdomain(boolean cdnSubdomain) {
+				this.conf.cdnSubdomain = cdnSubdomain;
+				return this;
+			}
+
+			public Builder setShorten(boolean shorten) {
+				this.conf.shorten = shorten;
+				return this;
+			}
+
+			public Builder setUploadPrefix(String uploadPrefix) {
+				this.conf.uploadPrefix = uploadPrefix;
+				return this;
+			}
+
+		}
 	}
 	
 	public Cloudinary(Map config) {
