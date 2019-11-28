d4dc23cc705d23cee1f1568feb0f9f99129433b2
Barak Aburus <baraka@gini-apps.com>
2019-11-28 11:45:22 +0200

    Prevent UploadWidgetActivity to restart when a configuration change occur

--- lib/src/main/java/com/cloudinary/android/uploadwidget/ui/UploadWidgetActivity.java
+++ lib/src/main/java/com/cloudinary/android/uploadwidget/ui/UploadWidgetActivity.java
@@ -3,6 +3,8 @@ package com.cloudinary.android.uploadwidget.ui;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 
@@ -13,6 +15,8 @@ import com.cloudinary.android.uploadwidget.UploadWidget;
  * Provides a solution out of the box for developers who want to use the Upload Widget.
  */
 public class UploadWidgetActivity extends AppCompatActivity implements UploadWidgetFragment.UploadWidgetListener {
+
+    private static final String UPLOAD_WIDGET_FRAGMENT_ID = "upload_widget_fragment_id";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,9 +29,13 @@ public class UploadWidgetActivity extends AppCompatActivity implements UploadWid
         }
 
         Uri imageUri = getIntent().getData();
-        UploadWidgetFragment uploadWidgetFragment = UploadWidgetFragment.newInstance(imageUri, this);
-        getSupportFragmentManager().beginTransaction()
-                .replace(R.id.container, uploadWidgetFragment)
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        Fragment fragment = fragmentManager.findFragmentByTag(UPLOAD_WIDGET_FRAGMENT_ID);
+        if (fragment == null) {
+            fragment = UploadWidgetFragment.newInstance(imageUri, this);
+        }
+        fragmentManager.beginTransaction()
+                .replace(R.id.container, fragment, UPLOAD_WIDGET_FRAGMENT_ID)
                 .commit();
     }
 
