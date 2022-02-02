ead15044937c4ba18b38f9534d1fe200b457e5e7
adimiz1 <adi.mizrahi@cloudinary.com>
2022-02-02 14:57:42 +0200

    Add analytics tests

---
+++ core/src/androidTest/java/com/cloudinary/android/AnalyticsTest.java
@@ -0,0 +1,42 @@
+package com.cloudinary.android;
+
+import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner;
+import androidx.test.platform.app.InstrumentationRegistry;
+
+import com.cloudinary.Cloudinary;
+import com.cloudinary.Transformation;
+import com.cloudinary.android.signed.Signature;
+import com.cloudinary.android.signed.SignatureProvider;
+import com.cloudinary.utils.StringUtils;
+
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.util.Map;
+
+@RunWith(AndroidJUnit4ClassRunner.class)
+public class AnalyticsTest extends AbstractTest {
+
+    private static boolean initialized = false;
+
+    @BeforeClass
+    public synchronized static void setup() {
+        MediaManager.get().getCloudinary().analytics.setSDKSemver("2.0.0");
+        MediaManager.get().getCloudinary().analytics.setTechVersion("13.0.0");
+    }
+
+    @Test
+    public void testAnalyticsURL() {
+        String url = MediaManager.get().getCloudinary().url().generate("sample");
+        Assert.assertEquals(url, "https://res.cloudinary.com/sdk-test/image/upload/sample?_a=AFAACAN0");
+    }
+
+    @Test
+    public void testAnalyticsOffURL() {
+        MediaManager.get().getCloudinary().config.analytics = false;
+        String url = MediaManager.get().getCloudinary().url().generate("sample");
+        Assert.assertEquals(url, "https://res.cloudinary.com/sdk-test/image/upload/sample");
+    }
+}
