7282cb9d4fdc690d10532632e24069dac5a56788
adimiz1 <adi.mizrahi@cloudinary.com>
2022-03-08 07:50:47 +0200

    Add test cloud script

--- tools/get_test_cloud.sh
+++ tools/get_test_cloud.sh
@@ -7,12 +7,12 @@ function test_cloud
    APIKEY=$(echo "${CLOUD_DETAILS}" |  python -c 'import json,sys;c=json.load(sys.stdin)["payload"];print("%s" % (c["cloudApiKey"]))')
    APISECRET=$(echo "${CLOUD_DETAILS}" |  python -c 'import json,sys;c=json.load(sys.stdin)["payload"];print("%s" % (c["cloudApiSecret"]))')
    CLOUD_NAME=$(echo "${CLOUD_DETAILS}" |  python -c 'import json,sys;c=json.load(sys.stdin)["payload"];print("%s" % (c["cloudName"]))')
-   export CLOUDINARY_URL=$(echo ${CLOUD_DETAILS} | python -c 'import json,sys;c=json.load(sys.stdin)["payload"];print("cloudinary://%s:%s@%s" % (c["cloudApiKey"], c["cloudApiSecret"], c["cloudName"]))')
+   CLOUDINARY_URL=$(echo ${CLOUD_DETAILS} | python -c 'import json,sys;c=json.load(sys.stdin)["payload"];print("cloudinary://%s:%s@%s" % (c["cloudApiKey"], c["cloudApiSecret"], c["cloudName"]))')
    curl \
   -d "name=cloudinary_java_test&unsigned=true" \
   -X POST \
   https://"${APIKEY}":"${APISECRET}"@api.cloudinary.com/v1_1/"${CLOUD_NAME}"/upload_presets
-
+  echo "${CLOUDINARY_URL}"
 }
 
 test_cloud
