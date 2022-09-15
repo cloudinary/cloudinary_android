f481197d0268ee94d502241225316827d7c8bee1
adimiz1 <adi.mizrahi@cloudinary.com>
2022-09-15 12:28:59 +0300

    Add unit testing to the new JobStrategy

--- core/src/androidTest/java/com/cloudinary/android/AndroidJobStrategyTest.java
+++ core/src/androidTest/java/com/cloudinary/android/AndroidJobStrategyTest.java
@@ -2,7 +2,13 @@ package com.cloudinary.android;
 
 
 import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner;
+import androidx.test.internal.util.ReflectionUtil;
+import androidx.work.BackoffPolicy;
+import androidx.work.Constraints;
+import androidx.work.OneTimeWorkRequest;
+import androidx.work.WorkManager;
 import androidx.work.WorkRequest;
+import androidx.work.impl.model.WorkSpec;
 
 import com.cloudinary.android.payload.FilePayload;
 
@@ -11,19 +17,27 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import java.io.IOException;
+import java.lang.reflect.Field;
+import java.lang.reflect.ParameterizedType;
 
 @RunWith(AndroidJUnit4ClassRunner.class)
 public class AndroidJobStrategyTest extends AbstractTest {
 
     @Test
-    public void testAdapter() throws InterruptedException, IOException {
+    public void testAdapter() throws InterruptedException, IOException, NoSuchFieldException, IllegalAccessException {
         FilePayload payload = buildPayload();
 
         int tenMinutes = 10 * 60 * 1000;
         UploadRequest<FilePayload> request = buildUploadRequest(payload, tenMinutes);
 
         WorkRequest adapted = AndroidJobStrategy.adapt(request);
+        Class obj = adapted.getClass().getSuperclass();
+        Field field = obj.getDeclaredField("mWorkSpec");
+        field.setAccessible(true);
+        Assert.assertEquals(true, adapted.getWorkSpec().constraints.requiresCharging());
+        Assert.assertEquals(false, adapted.getWorkSpec().constraints.requiresDeviceIdle());
+        Assert.assertEquals(10000, adapted.getWorkSpec().backoffDelayDuration);
+        Assert.assertEquals(BackoffPolicy.LINEAR, adapted.getWorkSpec().backoffPolicy);
 
-        Assert.assertNotNull(adapted.getId());
     }
 }
