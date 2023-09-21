867e421270f512dc18c50439bea89a27b626f453
adimiz1 <95848801+adimiz1@users.noreply.github.com>
2023-09-21 14:36:11 +0300

    Add video player widget

---
+++ ui/src/main/java/com/cloudinary/android/cldvideoplayer/CldVideoPlayer.java
@@ -0,0 +1,70 @@
+package com.cloudinary.android.cldvideoplayer;
+
+import android.content.Context;
+import android.util.Log;
+
+import com.cloudinary.Cloudinary;
+import com.cloudinary.Transformation;
+import com.cloudinary.android.MediaManager;
+import com.google.android.exoplayer2.ExoPlayer;
+import com.google.android.exoplayer2.MediaItem;
+
+import java.net.URL;
+
+
+public class CldVideoPlayer {
+
+    ExoPlayer player;
+
+    String url;
+
+    public CldVideoPlayer(Context context, URL url) {
+        this.url = url.toString();
+        initPlayer(context, this.url);
+    }
+
+    public CldVideoPlayer(Context context, String publicId) {
+        initiliaze(context, publicId, null, true);
+    }
+    public CldVideoPlayer(Context context, String publicId, Transformation transformation) {
+        initiliaze(context, publicId, transformation, true);
+    }
+
+    public CldVideoPlayer(Context context, String publicId, Transformation transformation, Boolean automaticStreamingProfile) {
+        initiliaze(context, publicId, transformation, automaticStreamingProfile);
+    }
+
+    private void initiliaze(Context context, String publicId, Transformation transformation, Boolean automaticStreamingProfile) {
+        if (automaticStreamingProfile && transformation == null) {
+            transformation = new Transformation();
+            transformation.streamingProfile("auto");
+            this.url = MediaManager.get().url().resourceType("video").transformation(transformation).format("m3u8").generate(publicId);
+        } else {
+            this.url = MediaManager.get().url().resourceType("video").transformation(transformation).generate(publicId);
+        }
+
+        initPlayer(context, url);
+    }
+
+    private void initPlayer(Context context, String url) {
+        player = new ExoPlayer.Builder(context).build();
+        player.setMediaItem(MediaItem.fromUri(url));
+        player.prepare();
+    }
+
+    public void play() {
+        if (player != null) {
+            player.play();
+        }
+    }
+
+    public void setPlayer(ExoPlayer player) {
+        this.player = player;
+    }
+
+    public ExoPlayer getPlayer() {
+        return player;
+    }
+
+    public String getUrl() { return this.url; }
+}
