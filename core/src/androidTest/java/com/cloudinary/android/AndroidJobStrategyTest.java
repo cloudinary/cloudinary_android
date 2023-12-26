92e512b71d9bd65d3aad2208016a293479ab97cf
adimiz1 <95848801+adimiz1@users.noreply.github.com>
2023-12-26 15:49:44 +0200

    Update AndroidJobStrategy to cancel requests

--- core/src/androidTest/java/com/cloudinary/android/AndroidJobStrategyTest.java
+++ core/src/androidTest/java/com/cloudinary/android/AndroidJobStrategyTest.java
@@ -3,6 +3,7 @@ package com.cloudinary.android;
 
 import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner;
 import androidx.test.internal.util.ReflectionUtil;
+import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.work.BackoffPolicy;
 import androidx.work.Constraints;
 import androidx.work.OneTimeWorkRequest;
@@ -10,8 +11,13 @@ import androidx.work.WorkManager;
 import androidx.work.WorkRequest;
 import androidx.work.impl.model.WorkSpec;
 
+import com.cloudinary.Cloudinary;
+import com.cloudinary.android.callback.ErrorInfo;
+import com.cloudinary.android.callback.UploadCallback;
 import com.cloudinary.android.payload.FilePayload;
+import com.cloudinary.utils.ObjectUtils;
 
+import org.cloudinary.json.JSONObject;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -20,10 +26,13 @@ import java.io.File;
 import java.io.IOException;
 import java.lang.reflect.Field;
 import java.lang.reflect.ParameterizedType;
+import java.util.Map;
 
 @RunWith(AndroidJUnit4ClassRunner.class)
 public class AndroidJobStrategyTest extends AbstractTest {
 
+    int success = 0;
+    int errors = 0;
     private File payloadFile;
 
     @Override
@@ -54,5 +63,40 @@ public class AndroidJobStrategyTest extends AbstractTest {
         Assert.assertEquals(10000, adapted.getWorkSpec().backoffDelayDuration);
         Assert.assertEquals(BackoffPolicy.LINEAR, adapted.getWorkSpec().backoffPolicy);
 
+    }
+    @Test
+    public void testCancelRequest() throws InterruptedException, IOException, NoSuchFieldException, IllegalAccessException {
+        FilePayload payload = buildPayload();
+        String requestId = MediaManager.get().upload(payload)
+                .unsigned(TEST_PRESET).callback(new UploadCallback() {
+                    @Override
+                    public void onStart(String requestId) {
+                    }
+
+                    @Override
+                    public void onProgress(String requestId, long bytes, long totalBytes) {
+                    }
+
+                    @Override
+                    public void onSuccess(String requestId, Map resultData) {
+                        success++;
+                    }
+
+                    @Override
+                    public void onError(String requestId, ErrorInfo error) {
+                        errors++;
+                    }
+
+                    @Override
+                    public void onReschedule(String requestId, ErrorInfo error) {
+
+                    }
+                })
+                .dispatch();
+        Thread.sleep(1000);
+        MediaManager.get().cancelRequest(requestId);
+        Thread.sleep(3000);
+        Assert.assertTrue(success == 0);
+        Assert.assertTrue(errors == 1);
     }
 }
