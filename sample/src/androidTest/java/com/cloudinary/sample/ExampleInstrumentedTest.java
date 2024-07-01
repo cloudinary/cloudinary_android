014447ee5a214e33ccba8a47280fcd4fea2c64e1
adimiz1 <adi.mizrahi@cloudinary.com>
2024-07-01 12:26:46 +0300

    Fix tests

--- sample/src/androidTest/java/com/cloudinary/sample/ExampleInstrumentedTest.java
+++ sample/src/androidTest/java/com/cloudinary/sample/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.cloudinary.cloudinarysampleapp", appContext.getPackageName());
+        assertEquals("com.cloudinary.sample", appContext.getPackageName());
     }
 }
