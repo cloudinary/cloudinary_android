b48bc19dc6e11c398155de34ca52ec1b882cd9f9
adimiz1 <adi.mizrahi@gmail.com>
2025-06-19 09:56:18 +0300

    Fix get test cloud

--- tools/get_test_cloud.sh
+++ tools/get_test_cloud.sh
@@ -4,10 +4,10 @@ function test_cloud
 {
    CLOUD_DETAILS=$(curl -X POST  \-H 'Content-type:application/json' \https://sub-account-testing.cloudinary.com/create_sub_account \--data '{"prefix" : "android-test-cloud"}')
    
-   APIKEY=$(echo "${CLOUD_DETAILS}" |  python3 -c 'import json,sys;c=json.load(sys.stdin)["payload"];print("%s" % (c["cloudApiKey"]))')
-   APISECRET=$(echo "${CLOUD_DETAILS}" |  python3 -c 'import json,sys;c=json.load(sys.stdin)["payload"];print("%s" % (c["cloudApiSecret"]))')
-   CLOUD_NAME=$(echo "${CLOUD_DETAILS}" |  python3 -c 'import json,sys;c=json.load(sys.stdin)["payload"];print("%s" % (c["cloudName"]))')
-   CLOUDINARY_URL=$(echo ${CLOUD_DETAILS} | python3 -c 'import json,sys;c=json.load(sys.stdin)["payload"];print("cloudinary://%s:%s@%s" % (c["cloudApiKey"], c["cloudApiSecret"], c["cloudName"]))')
+   APIKEY=$(echo "${CLOUD_DETAILS}" |  python -c 'import json,sys;c=json.load(sys.stdin)["payload"];print("%s" % (c["cloudApiKey"]))')
+   APISECRET=$(echo "${CLOUD_DETAILS}" |  python -c 'import json,sys;c=json.load(sys.stdin)["payload"];print("%s" % (c["cloudApiSecret"]))')
+   CLOUD_NAME=$(echo "${CLOUD_DETAILS}" |  python -c 'import json,sys;c=json.load(sys.stdin)["payload"];print("%s" % (c["cloudName"]))')
+   CLOUDINARY_URL=$(echo ${CLOUD_DETAILS} | python -c 'import json,sys;c=json.load(sys.stdin)["payload"];print("cloudinary://%s:%s@%s" % (c["cloudApiKey"], c["cloudApiSecret"], c["cloudName"]))')
    curl \
    -d "name=cloudinary_java_test&unsigned=true" \
   -X POST \
